5ce1c0062ecffaeb12cafe0994084ee65bd5a7f3
jambtc <30697838+jambtc@users.noreply.github.com>
2023-03-28 08:35:09 +0200

    fix: Utils

--- protected/controllers/UsersController.php
+++ protected/controllers/UsersController.php
@@ -6,7 +6,7 @@ Yii::import('libs.NaPacks.WebApp');
 Yii::import('libs.NaPacks.SaveModels'); // viene prima di Save
 Yii::import('libs.NaPacks.Save');
 Yii::import('libs.NaPacks.Push');
-Yii::import('libs.NaPacks.Utils');
+Yii::import('libs.Utils');
 
 class UsersController extends Controller
 {
